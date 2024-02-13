e7a1af7cb419f17bcad5ca9ace1f233a7ae22047
seorinnn <srgongju@naver.com>
2024-02-13 15:29:55 +0900

    WebtoonList 추가

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@ import MySwiper from './components/MySwiper';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { GluestackUIProvider, Center, Text, Box, VStack } from '@gluestack-ui/themed';
 import { config } from '@gluestack-ui/config'; // Optional if you want to use default theme
-import MediumCard from './components/Card/MediumCard';
+import WebtoonList from './components/WebtoonList';
 
 const queryClient= new QueryClient();
 
@@ -18,15 +18,8 @@ export default function App() {
           <Center width='100%' height={350}>
             <MySwiper/>
           </Center>
-          <VStack bg='$rose200' height={1000}>
-            <MediumCard 
-              webtoon={{
-              webtoonId:2000000002473,
-              title:"이번 생은 가주가 되겠습니다",
-              author:"ANTSTUDIO,몬(ANTSTUDIO)",
-              img:"https://kr-a.kakaopagecdn.com/P/C/2473/c1/2x/1edc518c-f131-41fd-b94c-431eb719ff00.png",
-              fanCount:124,
-            }}/>
+          <VStack bg='$rose200' height={1300}>
+            <WebtoonList/>
           </VStack>
           </ScrollView>
         </VStack>
---
+++ components/WebtoonList.tsx
@@ -0,0 +1,33 @@
+import { GluestackUIProvider, Center, Image, Text, Box, VStack } from '@gluestack-ui/themed';
+import { config } from '@gluestack-ui/config';
+import Swiper from 'react-native-swiper';
+import {Dimensions} from 'react-native';
+import { useEffect } from 'react';
+import {useQuery} from '@tanstack/react-query';
+import {Webtoon} from '../types';
+import MediumCard from './Card/MediumCard';
+
+//데이터타입 지정
+interface WebtoonResponse {
+  webtoons: Webtoon[];
+}
+
+//데이터 가져오기
+  const fetchWebtoons= async() => {
+    const res= await fetch('https://korea-webtoon-api.herokuapp.com');
+    return res.json();
+  };
+
+export default function WebtoonList() {
+    const { data } = useQuery<WebtoonResponse> ({
+      queryKey: ['korea-webtoon-api.herokuapp.com'],
+      queryFn: fetchWebtoons,
+    });
+
+  return (
+    <VStack>
+      {data
+        ? data.webtoons.map((webtoon)=> <MediumCard webtoon={webtoon}/>): [] }
+    </VStack>
+  );
+}
